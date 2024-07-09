4f5f2543df9a7c929a68ec1d5efafe3950447119
LauraWirtz <lw.laura.wirtz@gmail.com>
2024-07-09 13:13:19 +0200

    added tlp + wired mouse

--- etc/nixos/configuration-laptop.nix
+++ etc/nixos/configuration-laptop.nix
@@ -8,7 +8,10 @@
   boot.kernelParams = [ "psmouse.synatics_intertouch=0" ];
 
   services.logind.lidSwitch = "hibernate";
-  environment.systemPackages = [
-    pkgs.tlp
-  ];
+  services.tlp.enable = true;
+  services.tlp.settings = {
+	START_CHARGE_THRESH_BAT0=60;
+	STOP_CHARGE_THRESH_BAT0=80;
+  };
+  services.flatpak.enable = true;
 }
