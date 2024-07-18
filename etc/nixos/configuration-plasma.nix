bf80e3a9e46080b105a463b99699ae45e2b98be4
LauraWirtz <lw.laura.wirtz@gmail.com>
2024-07-18 20:39:59 +0200

    new hypr decorations + monitor layout

--- etc/nixos/configuration-plasma.nix
+++ etc/nixos/configuration-plasma.nix
@@ -2,6 +2,7 @@
   services.desktopManager.plasma6.enable = true;
 #  services.displayManager.defaultSession = "plasma";
   services.xserver.enable = true;
+#  services.displayManager.sddm.wayland.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -25,9 +25,8 @@
           ./configuration-pc.nix
           ./configuration-hyprland.nix
           ./configuration-plasma.nix
-#          ./configuration-greetd-plasma.nix
+          ./configuration-sddm.nix
 #          ./configuration-gnome.nix
-#          ./configuration-x11.nix
           ./hardware-pc.nix
           home-manager.nixosModules.home-manager {
             home-manager.users.laura = { pkgs, ... }: {
