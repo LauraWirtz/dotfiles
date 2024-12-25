c8447efe1bc991ce75f2f500f6509e6e5a2ce789
LauraWirtz <lw.laura.wirtz@gmail.com>
2024-12-25 17:02:21 +0100

    moved yuzu overlay into outputs

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -5,19 +5,25 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/master";
 		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-	nixpkgs-yuzu.url = "github:NixOS/nixpkgs/619c4b605e267f2eae56458d72070d4726ae1a31";
 	};
 
-	outputs = { self, nixpkgs, home-manager, nixpkgs-yuzu, ... }:
+	outputs = { self, nixpkgs, home-manager, ... }:
 		let
 			system = "x86_64-linux";
-			overlay-yuzu = final: prev: { yuzu = import nixpkgs-yuzu { inherit system; }; };
 		in {
 			nixosConfigurations.laura-pc = nixpkgs.lib.nixosSystem {
 				inherit system;
 				modules = [
-					({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-yuzu ]; })
+					({ config, pkgs, ... }: {
+						nixpkgs.overlays = [
+							(final: prev: {
+								yuzu = import (builtins.fetchGit {
+									url = "https://github.com/NixOS/nixpkgs/";
+									rev = "619c4b605e267f2eae56458d72070d4726ae1a31";
+								}) { inherit system; };
+							})
+						];
+					})
 					./configuration.nix
 					./configuration-pc.nix
 					./configuration-hyprland.nix
