ffe955a123530c472b7bb670520ad23875b585b5
LauraWirtz <lw.laura.wirtz@gmail.com>
2025-12-21 21:15:53 +0100

    waybar options-in-overview design

--- niri/home.nix
+++ niri/home.nix
@@ -5,6 +5,7 @@
 		".config/waybar/config.jsonc".source = with pkgs; config.lib.file.mkOutOfStoreSymlink "/etc/nixos/niri/.config/waybar/config.jsonc";
 		".config/waybar/style.css".source = with pkgs; config.lib.file.mkOutOfStoreSymlink "/etc/nixos/niri/.config/waybar/style.css";
 		".config/waybar/better-control-widget.sh".source = with pkgs; config.lib.file.mkOutOfStoreSymlink "/etc/nixos/niri/.config/waybar/better-control-widget.sh";
+		".config/waybar/overview.sh".source = with pkgs; config.lib.file.mkOutOfStoreSymlink "/etc/nixos/niri/.config/waybar/overview.sh";
 		".config/waybar/icons".source = with pkgs; config.lib.file.mkOutOfStoreSymlink "${pkgs.kdePackages.breeze-icons}/share/icons/breeze-dark/";
 	};
 }
