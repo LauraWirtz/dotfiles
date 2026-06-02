ad09c3256882d03ee3bef7af822e4cf101709fc6
LauraWirtz <lw.laura.wirtz@gmail.com>
2026-06-02 08:30:27 +0200

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "Laura Base Flake";
 
 	inputs = {
- 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+ 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
